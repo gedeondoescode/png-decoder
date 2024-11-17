8f2a6f234080be8ee257840cbfbfecd040e86815
Gedeon Sainrival <hello@gedeondoescode.com>
2024-11-16 19:24:47 -0500

    Identify PNG Chunks

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
 
+const decoderError = error{
+    NotPng,
+    EndOfFile,
+};
+
 // Check if the image is a PNG
 fn isPng(data: []const u8) bool {
 
@@ -24,17 +29,69 @@ fn isPng(data: []const u8) bool {
     return sig_value == data_value;
 }
 
+// define the chunk layout
+const ChunkLayout = struct {
+    length: usize,
+    type: []const u8,
+    data: []const u8,
+    crc: []const u8,
+};
+
+const PngDecoder = struct {
+    data: []const u8,
+    index: usize = 8, // Just trying something
+    complete: bool = false,
+
+    fn init(data: []const u8) !PngDecoder {
+        if (!isPng(data)) {
+            return decoderError.NotPng;
+        }
+
+        return .{ .data = data };
+    }
+
+    fn nextChunk(self: *PngDecoder) !?ChunkLayout {
+        if (self.complete == true) {
+            return null;
+        }
+
+        const bytes = try self.advanceChunk(4);
+        const length = std.mem.bigToNative(u32, std.mem.bytesToValue(u32, bytes));
+
+        const chunk_type = try self.advanceChunk(4);
+        if (std.mem.eql(u8, "IEND", chunk_type)) {
+            self.complete = true;
+        }
+
+        return .{ .length = length, .type = chunk_type, .data = try self.advanceChunk(length), .crc = try self.advanceChunk(4) };
+    }
+
+    fn advanceChunk(self: *PngDecoder, length: usize) ![]const u8 {
+        if (self.index + length > self.data.len) {
+            return decoderError.EndOfFile;
+        }
+
+        defer self.index += length;
+        return self.data[self.index .. self.index + length];
+    }
+};
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
     const alloc = gpa.allocator();
 
-    const png = try std.fs.cwd().openFile("screenshots.avif", .{});
+    // FIXME: CLI input based?
+    const png = try std.fs.cwd().openFile("sad_face.png", .{});
     defer png.close();
 
     const data = try png.readToEndAlloc(alloc, 1 * 1024 * 1024);
     defer alloc.free(data);
 
-    std.debug.print("Is a PNG: {}\n", .{isPng(data)});
+    var decoder = try PngDecoder.init(data);
+
+    while (try decoder.nextChunk()) |chunk| {
+        std.debug.print("Chunk Type: {s}\nChunk Length: {}\n", .{ chunk.type, chunk.length });
+    }
 }
